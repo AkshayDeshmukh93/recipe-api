47e8ec2a7c1d0c6b05e37bf3c5d38da5a259496c
AkshayDeshmukh93 <89724316+AkshayDeshmukh93@users.noreply.github.com>
2021-09-02 21:32:02 +0530

    Update SpringSecurityConfig.java

--- src/main/java/com/tcs/abn/recipeapi/SpringSecurityConfig.java
+++ src/main/java/com/tcs/abn/recipeapi/SpringSecurityConfig.java
@@ -1,15 +1,14 @@
 package com.tcs.abn.recipeapi;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+/*import org.springframework.http.HttpMethod;
 
-/*import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-*/
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;*/
+
 @Configuration
-public class SpringSecurityConfig 
-/* extends WebSecurityConfigurerAdapter */
+public class SpringSecurityConfig /* extends WebSecurityConfigurerAdapter */
 {
 
 	/*
@@ -23,11 +22,14 @@ public class SpringSecurityConfig
 	 * 
 	 * @Override protected void configure(HttpSecurity http) throws Exception {
 	 * 
-	 * http //HTTP Basic authentication .httpBasic() .and() .authorizeRequests()
-	 * .antMatchers(HttpMethod.GET, "/rest/recipe/**").hasRole("USER")
-	 * .antMatchers(HttpMethod.POST,"/rest/addrecipe").hasRole("USER") .and()
-	 * .csrf().disable() .formLogin().disable(); }
+	 * http.httpBasic() .and() .authorizeRequests() .antMatchers(HttpMethod.GET,
+	 * "/rest/recipe/**").hasRole("USER")
+	 * .antMatchers(HttpMethod.POST,"/rest/addrecipe/").hasRole("USER")
+	 * .antMatchers(HttpMethod.PUT,"/rest/updateRecipe/").hasRole("USER")
+	 * .antMatchers(HttpMethod.DELETE,"/rest/deleteRecipe/**").hasRole("USER")
+	 * .and() .csrf().disable() .formLogin().disable(); }
 	 */
+	 
 	
 
 }

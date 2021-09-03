61e67a99f80b758abbca44e92823fcd2c009da3e
AkshayDeshmukh93 <89724316+AkshayDeshmukh93@users.noreply.github.com>
2021-09-03 17:43:41 +0530

    Update RecipeApiApplicationTests.java

--- src/test/java/com/tcs/abn/recipeapi/RecipeApiApplicationTests.java
+++ src/test/java/com/tcs/abn/recipeapi/RecipeApiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.tcs.abn.recipeapi;
-
-
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.tcs.abn.recipeapi.controller.RecipeController;
@@ -15,7 +17,10 @@ class RecipeApiApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
+		RecipeApiApplication applicationClass=new RecipeApiApplication();
+		 assertThat(applicationClass).isEqualTo(applicationClass); 
+		    assertThat(applicationClass).hasSameHashCodeAs(applicationClass); 
 	}
+	
 
 }
